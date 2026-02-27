1b04b13b337df0c46966db5c29efe38616590947
virendra singh <rajputvirendra77172@gmail.com>
2026-02-27 17:12:10 +0530

    new commit

---
+++ src/main/java/net/dcode/myinsta/myinsta/MyinstaApplication.java
@@ -0,0 +1,13 @@
+package net.dcode.myinsta.myinsta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyinstaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyinstaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/dcode/myinsta/myinsta/MyinstaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.dcode.myinsta.myinsta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyinstaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
