a47f5f7525be44fe6b173c264dd277c5844509a3
virendra singh <rajputvirendra77172@gmail.com>
2026-02-28 12:23:28 +0530

    V1 set

--- src/main/java/net/dcode/myinsta/myinsta/entity/UserRegister.java
+++ src/main/java/net/dcode/myinsta/myinsta/entity/UserRegister.java
@@ -1,17 +1,16 @@
 package net.dcode.myinsta.myinsta.entity;
 
-import org.antlr.v4.runtime.misc.NotNull;
 import org.springframework.data.annotation.Id;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+//import jakarta.persistence.EnumType;
+//import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
 import lombok.Data;
-import net.dcode.myinsta.myinsta.enums.Role;
+//import net.dcode.myinsta.myinsta.enums.Role;
 
 @Data
 @Table(name = "users")
@@ -21,14 +20,15 @@ public class UserRegister {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
+    //@NotBlank(message="Name is required")
     private String name;
 
+   // @Email(message="Invalid email")
     @Column(unique = true)
     private String email;
 
+  //  @NotBlank(message="Password required")
     private String password;
 
-   // @Enumerated(EnumType.STRING)
-    //private Role role;
 }
